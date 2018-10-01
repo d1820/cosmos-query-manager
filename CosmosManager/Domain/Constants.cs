bfbd821697e93fd261993ef67c4780d98009e796
D. Turco <dt1820@gmail.com>
2018-10-01 09:25:33 -0700

    updated

--- CosmosManager/Domain/Constants.cs
+++ CosmosManager/Domain/Constants.cs
@@ -2,10 +2,17 @@ namespace CosmosManager.Domain
 {
     public static class Constants
     {
-         public static class EventId
+        public static class EventId
         {
             public const int COSMOSDB = 99;
             public const int REQUEST_RESPONSE = 100;
+        }
+
+        public static class QueryTypes
+        {
+            public const string SELECT = "SELECT";
+            public const string UPDATE = "UPDATE";
+            public const string DELETE = "DELETE";
         }
     }
 }
--- CosmosManager/QueryRunners/SelectQueryRunner.cs
+++ CosmosManager/QueryRunners/SelectQueryRunner.cs
@@ -18,13 +18,14 @@ namespace CosmosManager.QueryRunners
 
         public bool CanRun(string queryType)
         {
-            return queryType.Equals("SELECT", StringComparison.InvariantCultureIgnoreCase);
+            return queryType.Equals(Constants.QueryTypes.SELECT, StringComparison.InvariantCultureIgnoreCase);
 
         }
         public async Task<bool> RunAsync(IDocumentStore documentStore, string databaseName, string collectionName, string sql, bool logStats, ILogger logger)
         {
             try
             {
+                _presenter.ResetStatsLog();
                 var results = await documentStore.ExecuteAsync(databaseName, collectionName,
                                                                        async (IDocumentExecuteContext context) =>
                                                                       {
@@ -51,4 +52,61 @@ namespace CosmosManager.QueryRunners
             }
         }
     }
+
+    //public class DeletQueryRunner : IQueryRunner
+    //{
+    //    private int MAX_DEGREE_PARRELL = 5;
+    //    public bool CanRun(string queryType)
+    //    {
+    //        return queryType.Equals(Constants.QueryTypes.DELETE, StringComparison.InvariantCultureIgnoreCase);
+
+    //    }
+
+    //    public async Task<bool> RunAsync(IDocumentStore documentStore, string databaseName, string collectionName, string sql, bool logStats, ILogger logger)
+    //    {
+    //        ////sql in this context is the list of ids to remove
+    //        //var ids = sql.Split(new[] { ', ' });
+
+    //        ////select the  record, to transaction cache
+    //        //foreach (var id in ids)
+    //        //{
+    //        //    var actionTransactionCacheBlock = new ActionBlock<object>(async request =>
+    //        //                                                       {
+    //        //                                                           await documentStore.ExecuteAsync(databaseName, collectionName,
+    //        //                                                                          async (IDocumentExecuteContext context) =>
+    //        //                                                                         {
+
+    //        //                                                                             var doc = await context.QueryById<object>(id);
+    //        //                                                                             //save doc to file
+
+    //        //                                                                         });
+    //        //                                                       },
+    //        //                                                       new ExecutionDataflowBlockOptions
+    //        //                                                       {
+    //        //                                                           MaxDegreeOfParallelism = MAX_DEGREE_PARRELL
+    //        //                                                       });
+
+    //        //    foreach (var inboundDto in inboundDtos)
+    //        //    {
+    //        //        importBlock.Post(inboundDto);
+    //        //    }
+
+    //        //    actionTransactionCacheBlock.Complete();
+
+    //        //    await actionTransactionCacheBlock.Completion;
+    //        //}
+
+
+    //        ////execute delete
+    //        //await documentStore.ExecuteAsync(databaseName, collectionName,
+    //        //                                                           async (IDocumentExecuteContext context) =>
+    //        //                                                          {
+
+    //        //                                                              context.DeleteAsync()
+    //        //                                                          });
+
+    //        //return true;
+    //    }
+
+    //    }
 }
