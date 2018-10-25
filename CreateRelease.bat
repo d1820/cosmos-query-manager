f0d419828fbd5f3341238c69012c4d30ed5df90a
D. Turco <dt1820@gmail.com>
2018-10-24 18:00:59 -0700

    fixed installer

--- CreateRelease.bat
+++ CreateRelease.bat
@@ -1,21 +1,20 @@
 set version=%1
 
-
 echo Publishing Cosmos Manager...
 
-del /f ".\Releases\CosmosManagerInstaller-%version%.exe"
+rem del /f ".\Releases\CosmosManagerInstaller-%version%.exe"
 
-rd /s /q ".\CosmosManager\publish"
+rem rd /s /q ".\CosmosManager\publish"
 
-msbuild .\CosmosManager\CosmosManager.csproj /target:clean /p:configuration=Release
+rem msbuild .\CosmosManager\CosmosManager.csproj /target:clean /p:configuration=Release
 
 REM msbuild .\CosmosManager\CosmosManager.csproj  /p:configuration=Release
 
-msbuild .\CosmosManager\CosmosManager.csproj /target:publish /p:Configuration=Release /p:VisualStudioVersion=15.0
+rem msbuild .\CosmosManager\CosmosManager.csproj /target:publish /p:Configuration=Release /p:VisualStudioVersion=15.0
 
-xcopy /s /y ".\CosmosManager\bin\Release\app.publish\*" ".\CosmosManager\publish\"
+rem xcopy /s /y ".\CosmosManager\bin\Release\app.publish\*" ".\CosmosManager\publish\"
 
-del /f ".\CosmosManager\publish\CosmosManager2019.exe"
+rem del /f ".\CosmosManager\publish\CosmosManager2019.exe"
 
 echo Creating Installer
 "C:\Program Files\7-Zip\7z.exe" a -sfx7z.sfx ".\Releases\CosmosManagerInstaller-%version%.exe" ".\CosmosManager\publish\*"
